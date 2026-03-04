f8ea6ab57e9b61d4b86ce16b940bab39947ac9a7
Hein Htet Zaw <h3inhtetzaw346@gmail.com>
2026-03-04 09:54:55 +0630

    added outputs and randomized ports

--- do/main.tf
+++ do/main.tf
@@ -1,3 +1,32 @@
+//Generate uuid for seeding
+resource "random_uuid" "seed" {}
+
+//Get random port numbers for outline and ovpn
+locals {
+  start_port = 49152
+  end_port   = 65536
+  block_size = 1024
+
+  block_starts = [for s in range(local.start_port, local.end_port, local.block_size) : s]
+
+  port_pool = flatten([
+    for s in local.block_starts :
+    range(s, min(s + local.block_size, local.end_port))
+  ])
+
+  port_strings = [for p in local.port_pool : tostring(p)]
+}
+
+//Shuffle the list and get 3 results
+resource "random_shuffle" "vpn_ports" {
+  input        = sensitive(local.port_strings)
+  result_count = 3
+
+  keepers = {
+    seed = random_uuid.seed.result
+  }
+}
+
 resource "digitalocean_ssh_key" "vpn-factory-key" {
   name = "vpn-factory-key"
   public_key = file("../ssh-keys/vpn-factory-key.pub")
